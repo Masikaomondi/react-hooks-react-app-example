42ec4d49c1541c931e624c7382832a53ae613221
Masika <masikaomondi@gmail.com>
2022-09-22 15:30:47 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,20 @@ function App() {
         HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
-      <ExampleComponent />
+      function TestComponent() {
+  return (
+    <div className="video">
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/5TbUxGZtwGI"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        title="time video"
+        allowFullScreen
+      ></iframe>
     </div>
+  );
   );
 }
 
